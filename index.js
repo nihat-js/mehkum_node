6d56f43c6a7e1ccf2510e64abd4088358a028c90
nihat-js <abdullazadenihat@gmail.com>
2024-03-13 03:55:12 +0400

    test method added

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const express
+const express = require("express");
+
+const app = express()
+
+app.get("/test",function(req,res){
+  res.send("test")
+});
+
+
+app.listen(3000,()=>{
+  console.log("Server started");
+})
